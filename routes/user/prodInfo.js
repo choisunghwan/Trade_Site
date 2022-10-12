d2464501ca77d2a66a95c2188ec2964ebf967c7b
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-10-12 17:40:55 +0900

    오라클db에서 특정 데이터만 가져오기

--- routes/user/prodInfo.js
+++ routes/user/prodInfo.js
@@ -1,14 +1,35 @@
-
 var express = require('express');
 var router = express.Router();
-// const db = require('./../db'); // db 모듈 추가
-/* GET home page. */
-router.get('/', function(req, res, next) {
 
-    res.render('user/prodInfo'); 
+var oracledb = require('oracledb');
+const {
+    ORACLE_CONFIG
+} = require("../../config/db");
+
+
+router.get('/', async function(req, res, next) {
+  const productId = req.query.productId == undefined ? 1 : req.query.productId;
+  results = await selectprodInfo(productId);
+  res.render('user/prodInfo', {
+    prodInfo: results
+   });
 
 });
 
+//select
+async function selectprodInfo(productId) {
 
+    let connection = await oracledb.getConnection(ORACLE_CONFIG);
+    var sql = " SELECT * FROM PRODUCT WHERE PRODUCT_ID = :product_id "
+
+    let options = {
+      outFormat: oracledb.OUT_FORMAT_OBJECT   // query result format
+        };
+    let result = await connection.execute(sql, [productId], options);
+
+    await connection.close();
+
+    return result.rows;
+}
 
 module.exports = router;
